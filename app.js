82887aeee86ff9963fcb41f8bfb0a8dcead534d7
Daniel <duquedk1@gtmail.com>
2021-07-04 22:38:24 -0500

    modificacion de app.js

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ let box = document.querySelector(".container")
 
 function changeColor(e){
     let colorOption = document.getElementById("colors").value
-    //console.log(e.target.tagName)
     if (e.target.tagName ==="DIV"){
         e.target.style.backgroundColor = colorOption
     }
 }
-
 box.addEventListener("click", changeColor, true)
