12aed15ca3896ff35619f2e728238ac134366b6e
Daniel <duquedk1@gtmail.com>
2021-07-06 22:36:22 -0500

    Modificaicon del codigo por lo aprendido en clase

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-let box = document.querySelector(".container")
+let boxes = document.querySelectorAll(".container div")
+console.log(boxes.length)
 
-function changeColor(e){
+for(let i=0;i<boxes.length;i++){
+    boxes[i].addEventListener("click", cambiarColor, true)
+}
+function cambiarColor(e){
     let colorOption = document.getElementById("colors").value
     if (e.target.tagName ==="DIV"){
         e.target.style.backgroundColor = colorOption
     }
 }
-box.addEventListener("click", changeColor, true)
+/*
+function cambiarColor(){
+    let colorOption = document.getElementById("colors")
+    
+        switch(colorOption){
+            case "Azul":
+                div.style.backgroundColor = "blue"
+                break
+            case "Amarillo":
+                div.style.backgroundColor = "Yellow"
+                break
+            case "Rojo":
+                div.style.backgroundColor = "red"
+                break
+            case "Verde":
+                div.style.backgroundColor = "green"
+                break
+            case "Morado":
+                div.style.backgroundColor = "purple"
+                break
+            case "Naranja":
+                div.style.backgroundColor = "orange"
+                break
+            case "Cafe":
+                div.style.backgroundColor = "brown"
+                break
+        }
+    }
+*/
+/*
+
+*/
